115df835d73b3b691087ea39bd2f216a99275bac
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-01-18 17:56:16 -0300

    The Player object was updated in accordance with object-orientation

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,45 @@
 class Player {
-	String name;
-	int score;
+	/*
+	 * When instantiating a new Player object,
+	 * the player's name must be passed as ar-
+	 * gument. For example:
+	 *
+	 * Player newPlayer = new Player("John");
+	 *
+	 * Tha name and score attributes are pri-
+	 * vate. To access them, use the methods
+	 * below:
+	 *
+	 * newPlayerName = newPlayer.getName();
+	 * newPlayerScore = newPlayer.getScore();
+	 *
+	 * The player's score can be changed.
+	 * However, the value will be incre-
+	 * mented, i.e. it's not possible to
+	 * decrease it. See below:
+	 *
+	 * System.out.println(newPlayerScore); // 5
+	 * newPlayer.setScore(5);
+	 * System.out.println(newPlayer.getScore()); // 10
+	 */
+
+	private String name;
+	private int score;
 
 	public Player(String name) {
 		this.name = name;
 		this.score = 0;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getScore() {
+		return this.score;
+	}
+
+	public void setScore(int score) {
+		this.score += score;
 	}
 }
