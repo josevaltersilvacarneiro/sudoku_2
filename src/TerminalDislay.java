2743aa332a2a3c93483955a1d2442f9206df9799
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-01-19 18:21:30 -0300

    A new class was created to present the game to the user in terminal mode

---
+++ src/TerminalDislay.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class TerminalDisplay extends Display {
+
+        public int getDimension() {
+                return 2;
+        }
+
+	public String[] getPlayers() {
+                String[] playerNames = {"Jose", "Maria"};
+                
+		return playerNames;
+        }
+
+	private int getNumber() {
+                int     number;
+
+                Scanner read;
+
+                read   = new Scanner(System.in);
+                number = read.nextInt();
+                
+                return number;
+        }
+	
+	public void update() {
+                String repr;
+
+                repr = "";
+                for (int i = 0; i < 30; i++)
+                        repr += "=";
+                repr += "\n";
+
+		for (int row = 0; row < this.getLength(); row++) {
+                        for (int column = 0; column < this.getLength(); column++) {
+                                int section, house;
+
+                                section = this.convertToSection(row, column);
+                                house   = this.convertToHouse(row, column);
+
+                                repr   += Integer.toString(this.board[section].index(house));
+                        }
+                        repr += "\n";
+                }
+
+                for (int i = 0; i < 30; i++)
+                        repr += "=";
+
+                System.out.println(repr);
+        }
+}
