ed3ab4eb3a72feb241779c1c213458dda539983e
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-01-20 18:01:19 -0300

    Changing the Board initialization

--- src/Board.java
+++ src/Board.java
@@ -3,14 +3,22 @@ class Board {
 	int length;
 	Section board[];
 
+	int[] rowSum;
+	int[] columnSum;
+	int   diagonalSum;
+
 	public Board() {
 		this.dimension = 2;
 		this.length = this.dimension * this.dimension;
 		
-		board = new Section[this.length];
+		this.board = new Section[this.length];
 		
-		for (int i = 0; i < this.length; i++)
-			board[i] = new Section();
+		for (int i = 0; i < this.length; i++) {
+			this.board[i]     = new Section();
+			this.rowSum[i]    = 0;
+			this.columnSum[i] = 0;
+		}
+		this.diagonalSum	  = 0;
 	}
 
 	public Section getSection(int section) {
