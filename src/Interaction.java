afe19f02d6821d694ed6e37b69837023a46bf0d1
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-03-02 19:12:30 -0300

    Stylizing the code

--- src/Interaction.java
+++ src/Interaction.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 public abstract interface Interaction {
 	public abstract int getDimension();
         public abstract String[] getPlayers();
-        public abstract void update(ArrayList<Player> players);
         public abstract Move getMove(String playerName);
+        public abstract void update(ArrayList<Player> players);
 }
