71ab1c94f63261a349cbdb2f9e369b9f1e13dbce
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-03-02 19:23:13 -0300

    New comments were added to detail how the abstract interface works

--- src/Interaction.java
+++ src/Interaction.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 
 public abstract interface Interaction {
+	/**
+	 * This interface defines all interactions of the player
+	 * with the game.
+	 *
+	 * getDimension() returns the dimension typed by the player.
+	 * getPlayers()   returns the players who will participate in the match.
+	 * getMove()	  returns the player's move in the match.
+	 * update()	  refreshes the display.
+	 */
+
 	public abstract int getDimension();
         public abstract String[] getPlayers();
         public abstract Move getMove(String playerName);
